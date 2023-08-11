79ad5d0791bb5058ac05132e7e016e7cf6d7ce87
mazylol <landonp95@gmail.com>
2023-08-10 23:09:19 -0400

    formatted and nums hide when they are zero

--- src/App.svelte
+++ src/App.svelte
@@ -8,13 +8,13 @@
   let minutes: number;
   let seconds: number;
 
-  let x = setInterval(function() {
+  let x = setInterval(function () {
     let now = new Date().getTime();
 
     let distance = countDownDate - now;
 
     days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    hours = Math.floor((distance %  (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
@@ -28,23 +28,37 @@
 
   setInterval(() => {
     if (done) {
-      hue++
+      hue++;
     }
   }, 25);
 </script>
 
-<div style="--hue: {hue}" class="text-center text-white min-h-screen bg-neutral-800" class:rainbow={done}>
+<div
+  style="--hue: {hue}"
+  class="text-center text-white min-h-screen bg-neutral-800"
+  class:rainbow={done}
+>
   <div class="pt-[40vh]">
     {#if !done}
-    <div class="text-8xl font-bold flex flex-row flex-wrap justify-center gap-8">
-      <p>{days}d</p>
-      <p>{hours}h</p>
-      <p>{minutes}m</p>
-      <p>{seconds}s</p>
-    </div>
-  {:else}
-    <p class="text-7xl font-extrabold">GREAT DANE TIME GOGOGOGOGOGOGO</p>
-  {/if}
+      <div
+        class="text-8xl font-bold flex flex-row flex-wrap justify-center gap-8"
+      >
+        {#if days > 0}
+          <p>{days}d</p>
+        {/if}
+        {#if hours > 0}
+          <p>{hours}h</p>
+        {/if}
+        {#if minutes > 0}
+          <p>{minutes}m</p>
+        {/if}
+        {#if seconds > 0}
+          <p>{seconds}s</p>
+        {/if}
+      </div>
+    {:else}
+      <p class="text-7xl font-extrabold">GREAT DANE TIME GOGOGOGOGOGOGO</p>
+    {/if}
   </div>
 </div>
 
